2404bec1388983f26ddfe6d2dda448caaf07119e
Artem <meitaartem@gmail.com>
2020-01-28 14:48:04 +0200

    Add Paginator

--- blog/models.py
+++ blog/models.py
@@ -36,6 +36,9 @@ class Post(models.Model):
     def get_delete_url(self):
         return reverse('post_delete_url', kwargs={'slug': self.slug})
 
+    class Meta:
+        ordering = ['-date_pub']
+
 
 
 class Tag(models.Model):
@@ -56,3 +59,6 @@ class Tag(models.Model):
 
     def get_delete_url(self):
         return reverse('tag_delete_url', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['title']
